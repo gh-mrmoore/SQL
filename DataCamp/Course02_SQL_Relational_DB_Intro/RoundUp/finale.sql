7d6e0bd2071ad230b962cb0e1c8acbc6e4255279
mrm <matt.rmoore@gmail.com>
2020-09-12 21:46:15 -0500

    DataCamp Course 2 complete

---
+++ DataCamp/Course02_SQL_Relational_DB_Intro/RoundUp/finale.sql
@@ -0,0 +1,10 @@
+-- Count the total number of affiliations per university
+SELECT COUNT(*) /*AS count*/, professors.university_id 
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+-- Group by the ids of professors
+GROUP BY professors.university_id 
+ORDER BY count DESC;
+
+
